ba39faf063d04f7fe1c0f96136ad69b2ae0ef51f
poke30744 <poke30774@gmail.com>
2020-04-15 12:31:43 +0900

    initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # rpi-fan
 Python script to control the fan on Raspberry Pi
+
+> usage: fan.py [-h] --gpio GPIO --on ON --off OFF [--interval INTERVAL]
+> 
+> Python script to control fan on Raspberry Pi
+> 
+> optional arguments:
+>   -h, --help            show this help message and exit
+>   --gpio GPIO, -g GPIO  GPIO number
+>   --on ON, -n ON        ON threshold to start (in Celsius)
+>   --off OFF, -f OFF     OFF threshold to stop (in Celsius)
+>   --interval INTERVAL, -i INTERVAL
+>                         How often we check CPU temperature (seconds)
---
+++ fan.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import argparse
+import time
+from gpiozero import OutputDevice, CPUTemperature
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Python script to control fan on Raspberry Pi')
+    parser.add_argument('--gpio', '-g', required=True, type=int, help='GPIO pin number (BCM)')
+    parser.add_argument('--on', '-n', required=True, type=float, help='ON threshold to start (in Celsius)')
+    parser.add_argument('--off', '-f', required=True, type=float, help='OFF threshold to stop (in Celsius)')
+    parser.add_argument('--interval', '-i', default=5, type=int, help='How often we check CPU temperature (seconds)')
+    args = parser.parse_args()
+
+    fan = OutputDevice(args.gpio)
+    cpu = CPUTemperature()
+
+    while True:
+        if cpu.temperature > args.on:
+            fan.on()
+        elif cpu.temperature < args.off:
+            fan.off()
+        time.sleep(args.interval)
