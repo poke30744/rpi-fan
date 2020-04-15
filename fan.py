e10d60aa35ba86c8b71953824d4347e6f6884655
poke30744 <poke30774@gmail.com>
2020-04-15 13:48:43 +0900

    To use pin name (e.g.: WPI0, GPIO17, BCM17) instead of BCM number.

--- fan.py
+++ fan.py
@@ -5,7 +5,7 @@ from gpiozero import OutputDevice, CPUTemperature
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Python script to control fan on Raspberry Pi')
-    parser.add_argument('--gpio', '-g', required=True, type=int, help='GPIO pin number (BCM)')
+    parser.add_argument('--gpio', '-g', required=True, help='GPIO pin name (e.g.: GPIO17, BCM17, BOARD11, WPI0, J8:11)')
     parser.add_argument('--on', '-n', required=True, type=float, help='ON threshold to start (in Celsius)')
     parser.add_argument('--off', '-f', required=True, type=float, help='OFF threshold to stop (in Celsius)')
     parser.add_argument('--interval', '-i', default=5, type=int, help='How often we check CPU temperature (seconds)')
